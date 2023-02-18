11447c7215df2dd03445d98e86e708d27f09ea55
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-02-18 14:37:34 +0600

    parallelgram

--- geometry.js
+++ geometry.js
@@ -24,16 +24,52 @@ document.getElementById('rectangle-btn').addEventListener('click', function(){
   const rectangleWidthText = rectangleWidth.value;
   const rectangleWidthTextInNumber = parseFloat(rectangleWidthText)
 
-const rectangleLength = document.getElementById('rectangle-length');
-const rectangleLengthText  = rectangleLength.value;
-const rectangleLengthTextInNumber =  parseFloat(rectangleLengthText);
+  const rectangleLength = document.getElementById('rectangle-length');
+  const rectangleLengthText  = rectangleLength.value;
+  const rectangleLengthTextInNumber =  parseFloat(rectangleLengthText);
 
 
-const rectangleArea = (rectangleWidthTextInNumber * rectangleLengthTextInNumber);
-const rectangleAreaInTwoDeciaml = rectangleArea.toFixed(2);
+  const rectangleArea = (rectangleWidthTextInNumber * rectangleLengthTextInNumber);
+  const rectangleAreaInTwoDeciaml = rectangleArea.toFixed(2);
 
-const ol = document.getElementById("calculation-area");
+  const ol = document.getElementById("calculation-area");
+    const li = document.createElement("li");
+    li.innerText = "Rectangle Area " + " " + rectangleAreaInTwoDeciaml;
+    ol.appendChild(li)
+})
+
+//parallelogram
+document.getElementById('parallelogram-btn').addEventListener('click', function(){
+  const parallelogramBase = document.getElementById('parallelgram-base');
+  const parallelgramBaseText = parallelogramBase.value;
+  const parallelgramBaseTextInNumber = parseFloat(parallelgramBaseText)
+
+  const parallelgramHeignt = document.getElementById('parallelgram-height');
+  const parallelgramHeigntText = parallelgramHeignt.value;
+  const parallelgramHeigntTextInNumber =parseFloat(parallelgramHeigntText);
+
+  const parallelgramArea = (parallelgramBaseTextInNumber * parallelgramHeigntTextInNumber).toFixed(2);
+
+  const ol = document.getElementById("calculation-area");
   const li = document.createElement("li");
-  li.innerText = "Rectangle Area " + " " + rectangleAreaInTwoDeciaml;
+  li.innerText = "parallelgramArea" + " " + parallelgramArea;
+  ol.appendChild(li)
+})
+
+//Rhombus
+document.getElementById('rhombus-btn').addEventListener('click', function(){
+  const rhombusD1 = document.getElementById('rhombus-d1');
+  const rhombusD1Text = rhombusD1.value;
+  const rhombusD1InNumber = parseFloat(rhombusD1Text)
+
+  const rhombusD2 = document.getElementById('rhombus-d1');
+  const rhombusD2Text = rhombusD1.value;
+  const rhombusD2InNumber = parseFloat(rhombusD1Text)
+
+  const parallelgramArea = (parallelgramBaseTextInNumber * parallelgramHeigntTextInNumber).toFixed(2);
+
+  const ol = document.getElementById("calculation-area");
+  const li = document.createElement("li");
+  li.innerText = "parallelgramArea" + " " + parallelgramArea;
   ol.appendChild(li)
 })
