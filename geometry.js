6ee1de643057aad90950d3e79633dd7bbb189f50
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-02-18 13:28:00 +0600

    reactangle area

--- geometry.js
+++ geometry.js
@@ -1,20 +1,39 @@
+// triangle 
 
 document.getElementById('triangle-btn').addEventListener('click', function(){
   const triangleBase = document.getElementById('triangle-base');
   const triangleBaseText = triangleBase.value;
   const triangleBaseTextinNumber = parseFloat(triangleBaseText);
-  // const triangleBaseInTwoDecimal = triangleBaseTextinNumber.toFixed(2);
-
 
   const triangleHeight = document.getElementById('triangle-height');
   const triangleHeightText = triangleHeight.value;
   const triangleHeightTextInfloat = parseFloat(triangleHeightText)
-  // const triangleHeightInTwoDecimal = triangleHeightTextInfloat.toFixed(2)
-
 
   const triangleArea = (triangleBaseTextinNumber * triangleHeightTextInfloat)/2;
   const triangleAreaAreaInTwoDecimal = triangleArea.toFixed(2)
 
+  const ol = document.getElementById("calculation-area");
+  const li = document.createElement("li");
+  li.innerText = "triangle" + " " + triangleAreaAreaInTwoDecimal;
+  ol.appendChild(li)
+})
 
-  console.log(triangleAreaAreaInTwoDecimal)
+//rectangle
+document.getElementById('rectangle-btn').addEventListener('click', function(){
+  const rectangleWidth = document.getElementById('rectangle-width');
+  const rectangleWidthText = rectangleWidth.value;
+  const rectangleWidthTextInNumber = parseFloat(rectangleWidthText)
+
+const rectangleLength = document.getElementById('rectangle-length');
+const rectangleLengthText  = rectangleLength.value;
+const rectangleLengthTextInNumber =  parseFloat(rectangleLengthText);
+
+
+const rectangleArea = (rectangleWidthTextInNumber * rectangleLengthTextInNumber);
+const rectangleAreaInTwoDeciaml = rectangleArea.toFixed(2);
+
+const ol = document.getElementById("calculation-area");
+  const li = document.createElement("li");
+  li.innerText = "Rectangle Area " + " " + rectangleAreaInTwoDeciaml;
+  ol.appendChild(li)
 })
